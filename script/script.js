22cb402bec2ea76a9ca6ac78908bcdc360c93e2f
pavelKuklin <pavelkuklinweb@gmail.com>
2020-02-20 17:51:11 +0500

    повторил за Максимом

--- script/script.js
+++ script/script.js
@@ -1,38 +1,87 @@
-window.addEventListener('DOMContentLoaded', function () {
-  'use strict'
+window.addEventListener('DOMContentLoaded', function() {
+    'use strict'
 
-  //Timer
-  function countTimer(deadline) {
-    let timerHours = document.querySelector('#timer-hours'),
-      timerMinutes = document.querySelector('#timer-minutes'),
-      timerSeconds = document.querySelector('#timer-seconds');
+    //Timer
+    function countTimer(deadline) {
+        let timerHours = document.querySelector('#timer-hours'),
+            timerMinutes = document.querySelector('#timer-minutes'),
+            timerSeconds = document.querySelector('#timer-seconds');
 
-    function getTimeRemaining() {
-      let dateStop = new Date(deadline).getTime(),
-        dateNow = new Date().getTime(),
-        timeRemaining = (dateStop - dateNow) / 1000,
-        seconds = Math.floor(timeRemaining % 60).toString().padStart(2, "0"),
-        minutes = Math.floor((timeRemaining / 60) % 60).toString().padStart(2, "0"),
-        hours = Math.floor((timeRemaining / 60 / 60) % 24).toString().padStart(2, "0"),
-        day = Math.floor(timeRemaining / 60 / 60 / 24).toString().padStart(2, "0");
-      if (timeRemaining <= 0) {
-        seconds = '00';
-        hours = '00';
-        minutes = '00';
-      }
+        function getTimeRemaining() {
+            let dateStop = new Date(deadline).getTime(),
+                dateNow = new Date().getTime(),
+                timeRemaining = (dateStop - dateNow) / 1000,
+                seconds = Math.floor(timeRemaining % 60).toString().padStart(2, "0"),
+                minutes = Math.floor((timeRemaining / 60) % 60).toString().padStart(2, "0"),
+                hours = Math.floor((timeRemaining / 60 / 60) % 24).toString().padStart(2, "0"),
+                day = Math.floor(timeRemaining / 60 / 60 / 24).toString().padStart(2, "0");
+            if (timeRemaining <= 0) {
+                seconds = '00';
+                hours = '00';
+                minutes = '00';
+            }
 
-      return { timeRemaining, hours, minutes, seconds, };
+            return { timeRemaining, hours, minutes, seconds, };
+        }
+
+        function updateCloack() {
+            let timer = getTimeRemaining()
+
+            timerHours.textContent = timer.hours;
+            timerMinutes.textContent = timer.minutes;
+            timerSeconds.textContent = timer.seconds;
+        }
+
+        setInterval(updateCloack, 1000);
     }
 
-    function updateCloack() {
-      let timer = getTimeRemaining()
+    countTimer('23 february 2020');
 
-      timerHours.textContent = timer.hours;
-      timerMinutes.textContent = timer.minutes;
-      timerSeconds.textContent = timer.seconds;
-    }
+    //меню
+    const toggleMenu = () => {
 
-    setInterval(updateCloack, 1000);
-  }
-  countTimer('23 february 2020')
+        const btnMenu = document.querySelector('.menu'),
+            menu = document.querySelector('menu'),
+            closeBtn = document.querySelector('.close-btn'),
+            menuItems = menu.querySelectorAll('ul>li');
+
+        const handlerMenu = () => {
+            menu.classList.toggle('active-menu');
+        };
+
+        btnMenu.addEventListener('click', () => {
+            handlerMenu();
+
+        });
+
+        closeBtn.addEventListener('click', () => {
+            handlerMenu();
+        });
+
+        menuItems.forEach(element => {
+            element.addEventListener('click', handlerMenu)
+        })
+    };
+
+    toggleMenu();
+
+    //popUP
+    const togglePopUp = () => {
+        const popup = document.querySelector('.popup'),
+            popupBtn = document.querySelectorAll('.popup-btn'),
+            popupClose = document.querySelector('.popup-close');
+
+        popupBtn.forEach(item => {
+            item.addEventListener('click', () => {
+                popup.style.display = 'block';
+            });
+        });
+
+        popupClose.addEventListener('click', () => {
+            popup.style.display = 'none';
+        });
+
+    };
+
+    togglePopUp();
 });
