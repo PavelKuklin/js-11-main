650d683941a177b58801b3d88dba0e6bf10d0357
Pavel Kuklin <trb34074@yandeex.ru>
2020-02-20 06:52:11 +0500

    делаю сложное задание, сохраняю

--- lesson16_1/script2.js
+++ lesson16_1/script2.js
@@ -1,46 +1,93 @@
-
 'use strict'
+const start = document.querySelector('.start'),
+  reset = document.querySelector('.reset'),
+  box = document.querySelector('.box');
 
-function welcome() {
-  const container = document.querySelector('.container');
+let game = false,
+  left = 100,
+  gameAnimate = '';
 
-  let date = new Date();
-
-  function getTimeOfDay(date) {
-    let timeOfDay = date.getHours();
-
-    if (timeOfDay > 6 && timeOfDay < 12) {
-      return 'утро';
-    } else if (timeOfDay > 12 && timeOfDay < 18) {
-      return 'день';
-    } else if (timeOfDay > 18 && timeOfDay < 24) {
-      return 'вечер';
-    } else if (timeOfDay > 0 && timeOfDay < 6) {
-      return 'ночи';
+let gaming = function () {
+  if (game) {
+    if (left < 1600) {
+      gameAnimate = requestAnimationFrame(gaming);
+      left = left + 10;
+      box.style.left = `${left}px`;
     }
+  } else {
+    cancelAnimationFrame(gameAnimate);
   }
+};
 
-  function timeToDate(date) {
-    let currentDate = new Date().getTime(),
-      nextDate = new Date(date).getTime();
-
-    return Math.floor((nextDate - currentDate) / 1000 / 60 / 60 / 24);
+let startAnimations = function () {
+  if (!game) {
+    game = true
+    gaming();
+  } else {
+    game = false;
+    gaming();
   }
+};
 
-  function getWeeksDay(date) {
-    return date.toLocaleString('ru', {
-      'weekday': 'long',
-    })
-  }
+let resetGame = function () {
+  game = false;
+  gaming();
+  left = 100;
+  box.style.left = left + 'px';
+};
 
-  container.innerHTML = `Добрый ${getTimeOfDay(date)} <br>
-  Сегодня: ${getWeeksDay(date)}<br>
-  Текущее время: ${date.toLocaleTimeString('en')}<br>
-  До нового года осталось ${timeToDate('31 december 2020')} дней
-  `;
-}
+start.addEventListener('click', startAnimations);
+reset.addEventListener('click', resetGame);
 
-welcome();
+let date = new Date();
+let date2 = date.getDate() + 1;
+let date3 = date;
+date3 = date3.setDate(1);
+
+console.log(Math.floor(date3));
+
+
+
+// function welcome() {
+//   const container = document.querySelector('.container');
+
+//   let date = new Date();
+
+//   function getTimeOfDay(date) {
+//     let timeOfDay = date.getHours();
+
+//     if (timeOfDay > 6 && timeOfDay < 12) {
+//       return 'утро';
+//     } else if (timeOfDay > 12 && timeOfDay < 18) {
+//       return 'день';
+//     } else if (timeOfDay > 18 && timeOfDay < 24) {
+//       return 'вечер';
+//     } else if (timeOfDay > 0 && timeOfDay < 6) {
+//       return 'ночи';
+//     }
+//   }
+
+//   function timeToDate(date) {
+//     let currentDate = new Date().getTime(),
+//       nextDate = new Date(date).getTime();
+
+//     return Math.floor((nextDate - currentDate) / 1000 / 60 / 60 / 24);
+//   }
+
+//   function getWeeksDay(date) {
+//     return date.toLocaleString('ru', {
+//       'weekday': 'long',
+//     })
+//   }
+
+//   container.innerHTML = `Добрый ${getTimeOfDay(date)} <br>
+//   Сегодня: ${getWeeksDay(date)}<br>
+//   Текущее время: ${date.toLocaleTimeString('en')}<br>
+//   До нового года осталось ${timeToDate('31 december 2020')} дней
+//   `;
+// }
+
+//welcome();
 
 
 
