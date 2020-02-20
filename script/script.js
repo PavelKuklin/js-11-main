11adac80f9252531cb8e93dd6494473ac0ec6f7d
Pavel Kuklin <trb34074@yandeex.ru>
2020-02-20 20:49:04 +0500

    обязательное задание 18 урок

--- script/script.js
+++ script/script.js
@@ -1,87 +1,103 @@
-window.addEventListener('DOMContentLoaded', function() {
-    'use strict'
+window.addEventListener('DOMContentLoaded', function () {
+  'use strict'
 
-    //Timer
-    function countTimer(deadline) {
-        let timerHours = document.querySelector('#timer-hours'),
-            timerMinutes = document.querySelector('#timer-minutes'),
-            timerSeconds = document.querySelector('#timer-seconds');
+  //Timer
+  function countTimer(deadline) {
+    let timerHours = document.querySelector('#timer-hours'),
+      timerMinutes = document.querySelector('#timer-minutes'),
+      timerSeconds = document.querySelector('#timer-seconds');
 
-        function getTimeRemaining() {
-            let dateStop = new Date(deadline).getTime(),
-                dateNow = new Date().getTime(),
-                timeRemaining = (dateStop - dateNow) / 1000,
-                seconds = Math.floor(timeRemaining % 60).toString().padStart(2, "0"),
-                minutes = Math.floor((timeRemaining / 60) % 60).toString().padStart(2, "0"),
-                hours = Math.floor((timeRemaining / 60 / 60) % 24).toString().padStart(2, "0"),
-                day = Math.floor(timeRemaining / 60 / 60 / 24).toString().padStart(2, "0");
-            if (timeRemaining <= 0) {
-                seconds = '00';
-                hours = '00';
-                minutes = '00';
-            }
+    function getTimeRemaining() {
+      let dateStop = new Date(deadline).getTime(),
+        dateNow = new Date().getTime(),
+        timeRemaining = (dateStop - dateNow) / 1000,
+        seconds = Math.floor(timeRemaining % 60).toString().padStart(2, "0"),
+        minutes = Math.floor((timeRemaining / 60) % 60).toString().padStart(2, "0"),
+        hours = Math.floor((timeRemaining / 60 / 60) % 24).toString().padStart(2, "0"),
+        day = Math.floor(timeRemaining / 60 / 60 / 24).toString().padStart(2, "0");
+      if (timeRemaining <= 0) {
+        seconds = '00';
+        hours = '00';
+        minutes = '00';
+      }
 
-            return { timeRemaining, hours, minutes, seconds, };
-        }
-
-        function updateCloack() {
-            let timer = getTimeRemaining()
-
-            timerHours.textContent = timer.hours;
-            timerMinutes.textContent = timer.minutes;
-            timerSeconds.textContent = timer.seconds;
-        }
-
-        setInterval(updateCloack, 1000);
+      return { timeRemaining, hours, minutes, seconds, };
     }
 
-    countTimer('23 february 2020');
+    function updateCloack() {
+      let timer = getTimeRemaining()
 
-    //меню
-    const toggleMenu = () => {
+      timerHours.textContent = timer.hours;
+      timerMinutes.textContent = timer.minutes;
+      timerSeconds.textContent = timer.seconds;
+    }
 
-        const btnMenu = document.querySelector('.menu'),
-            menu = document.querySelector('menu'),
-            closeBtn = document.querySelector('.close-btn'),
-            menuItems = menu.querySelectorAll('ul>li');
+    setInterval(updateCloack, 1000);
+  }
 
-        const handlerMenu = () => {
-            menu.classList.toggle('active-menu');
-        };
+  countTimer('23 february 2020');
 
-        btnMenu.addEventListener('click', () => {
-            handlerMenu();
+  //меню
+  const toggleMenu = () => {
 
-        });
+    const btnMenu = document.querySelector('.menu'),
+      menu = document.querySelector('menu'),
+      closeBtn = document.querySelector('.close-btn'),
+      menuItems = menu.querySelectorAll('ul>li');
 
-        closeBtn.addEventListener('click', () => {
-            handlerMenu();
-        });
-
-        menuItems.forEach(element => {
-            element.addEventListener('click', handlerMenu)
-        })
+    const handlerMenu = () => {
+      menu.classList.toggle('active-menu');
     };
 
-    toggleMenu();
+    btnMenu.addEventListener('click', () => {
+      handlerMenu();
 
-    //popUP
-    const togglePopUp = () => {
-        const popup = document.querySelector('.popup'),
-            popupBtn = document.querySelectorAll('.popup-btn'),
-            popupClose = document.querySelector('.popup-close');
+    });
 
-        popupBtn.forEach(item => {
-            item.addEventListener('click', () => {
-                popup.style.display = 'block';
-            });
-        });
+    closeBtn.addEventListener('click', () => {
+      handlerMenu();
+    });
 
-        popupClose.addEventListener('click', () => {
-            popup.style.display = 'none';
-        });
+    menuItems.forEach(element => {
+      element.addEventListener('click', handlerMenu)
+    })
+  };
 
+  toggleMenu();
+
+  //popUP
+  const togglePopUp = () => {
+    const popup = document.querySelector('.popup'),
+      popupBtn = document.querySelectorAll('.popup-btn'),
+      popupClose = document.querySelector('.popup-close'),
+      popupContent = document.querySelector('.popup-content')
+    let top = 10;
+    //анимация popup
+    const popupAnimation = () => {
+      if (document.documentElement.clientWidth > 768) {
+        if (top < 35) {
+          top++;
+          popupContent.style.cssText = `top: ${top}%;`
+          requestAnimationFrame(popupAnimation);
+        } else {
+          cancelAnimationFrame(popupAnimation);
+          top = 10;
+        }
+      }
     };
+    popupBtn.forEach(item => {
+      item.addEventListener('click', () => {
+        popup.style.display = 'block';
+        popupAnimation();
 
-    togglePopUp();
+      });
+    });
+
+    popupClose.addEventListener('click', () => {
+      popup.style.display = 'none';
+
+    });
+  };
+
+  togglePopUp();
 });
