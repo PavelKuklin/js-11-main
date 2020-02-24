8cce473056c0b6ee6a0b2b6025f407770ed98390
Pavel Kuklin <trb34074@yandeex.ru>
2020-02-24 16:43:17 +0500

    выполнил обязательное задание, на смену фото в блоке команда

--- script/script.js
+++ script/script.js
@@ -1,3 +1,4 @@
+/* eslint-disable indent */
 'use strict'
 window.addEventListener('DOMContentLoaded', () => {
   //Timer
@@ -314,5 +315,34 @@ window.addEventListener('DOMContentLoaded', () => {
   };
 
   calc();
+
+  //наша команда
+
+  const team = () => {
+    const comrad = document.querySelectorAll('.command__photo'),
+      command = document.querySelector('.command');
+
+    let targetSrc = '';
+
+    command.addEventListener('mouseover', () => {
+      const target = event.target;
+
+      if (target.matches('.command__photo')) {
+        targetSrc = target.getAttribute('src');
+        target.src = target.dataset.img;
+      }
+    });
+
+    command.addEventListener('mouseout', () => {
+      const target = event.target;
+
+      if (target.matches('.command__photo')) {
+        target.src = targetSrc;
+      }
+    });
+
+  };
+
+  team();
 
 });
