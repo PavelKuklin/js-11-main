b7995f09e00cce41ee9ad9a370ab0a458b689ff6
Pavel Kuklin <trb34074@yandeex.ru>
2020-02-25 21:10:24 +0500

    сделал обязательное задание

--- script/script.js
+++ script/script.js
@@ -297,9 +297,61 @@ window.addEventListener('DOMContentLoaded', () => {
   slider();
 
   //калькулятор 
-  const calc = () => {
+  const calc = (price = 100) => {
     const calcBlock = document.querySelector('.calc-block'),
-      calcInput = document.querySelectorAll('.calc-item');
+      calcInput = document.querySelectorAll('.calc-item'),
+      calcType = document.querySelector('.calc-type'),
+      calcSquare = document.querySelector('.calc-square'),
+      calcDay = document.querySelector('.calc-day'),
+      calcCount = document.querySelector('.calc-count'),
+      totalValue = document.getElementById('total');
+
+    const countSum = () => {
+      let total = 0,
+        countValue = 1,
+        dayValue = 1;
+      const typeValue = calcType.options[calcType.selectedIndex].value,
+        squareValue = +calcSquare.value;
+
+      if (calcCount.value > 1) {
+        countValue += (+calcCount.value - 1) / 10;
+      }
+
+      if (calcDay.value && calcDay.value < 5) {
+        dayValue *= 2;
+      } else if (calcDay.value && calcDay.value < 10) {
+        dayValue *= 1.5;
+      }
+
+      if (typeValue && squareValue) {
+        total = price * typeValue * squareValue * countValue * dayValue;
+      }
+      totalValue.textContent = total;
+
+      // if (total > 0) {
+      //   let newTotal = 0;
+      //   const step = () => {
+      //     setTimeout(() => {
+      //       newTotal++;
+      //       totalValue.textContent = newTotal;
+      //       if (newTotal < total) {
+      //         step();
+      //       }
+      //     }, 0.0001);
+      //   };
+      //   step();
+      //}
+      //
+    };
+
+
+    calcBlock.addEventListener('change', (event) => {
+      let target = event.target;
+
+      if (target.matches('select') || target.matches('input')) {
+        countSum();
+      }
+    });
 
     calcInput.forEach(item => {
       if (item.getAttribute('type' === 'number')) {
@@ -314,7 +366,7 @@ window.addEventListener('DOMContentLoaded', () => {
     });
   };
 
-  calc();
+  calc(100);
 
   //наша команда.
 
