040ff1265092b2898c0e77f92bf4ecd45ce8c458
Pavel Kuklin <trb34074@yandeex.ru>
2020-03-01 15:20:53 +0500

    загрузил часть урок, что бы не слетели настройки

--- script/script.js
+++ script/script.js
@@ -397,5 +397,46 @@ window.addEventListener('DOMContentLoaded', () => {
 
     team();
 
+    //send ajax form
+    const sendForm = () => {
+        const errorMessage = 'Что то пошло не так...',
+            loadMessage = 'Загрузка',
+            successMessage = 'Спасибо, мы скоро с вами свяжемся';
+
+        const form = document.getElementById('form1');
+
+        const statusMessage = document.createElement('div');
+        statusMessage.style.cssText = `font-size: 2rem;`
+
+        form.addEventListener('submit', (event) => {
+            event.preventDefault();
+            form.append(statusMessage);
+
+            const request = new XMLHttpRequest();
+            request.open('POST', './server.php');
+            request.setRequestHeader('Content-Type', 'multipart/form-data');
+            const formData = new FormData(form);
+            request.send(formData);
+
+
+            request.addEventListener('readystatechange', (event) => {
+                statusMessage.textContent = loadMessage;
+
+                if (request.readyState !== 4) {
+                    return;
+                }
+
+                if (request.status === 200) {
+                    statusMessage.textContent = successMessage;
+                } else {
+                    statusMessage.textContent = errorMessage;
+                }
+            });
+        });
+
+
+    };
+
+    sendForm();
 
 });
